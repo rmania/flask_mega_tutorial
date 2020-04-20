37fa5f39c0b1121bd2ae11caa15bdeff6b0b74d3
rmania <dmeijerink1979@gmail.com>
2020-04-20 10:28:26 +0200

    added sqlite db

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db) # # migration engine
 
 # workaround to circular imports
-from app import routes
+from app import routes, models # to define database structure
--- microblog/config.py
+++ microblog/config.py
@@ -1,5 +1,10 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     # value from ENV variable preferred, otherwise use hardcoded string
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+    # if ENV not defined fallback var configuring a database named app.db
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        "sqlite:///" + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
