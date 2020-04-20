828ac80882e091541cfa27747e138722d907df0f
rmania <dmeijerink1979@gmail.com>
2020-04-20 11:04:22 +0200

    wip

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -7,6 +7,8 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    # high-level view of the relationship between users and posts:
+    posts = db.relationship("Post", backref="author", lazy="dynamic")
 
     def __repr__(self):
         return f"<User {self.username}>"
