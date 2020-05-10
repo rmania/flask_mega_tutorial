14ec25cbd305b9774e70fc456727f5804a123469
rmania <dmeijerink1979@gmail.com>
2020-05-10 22:08:41 +0200

    error messages and bug fix duplicate user

---
+++ microblog/app/errors.py
@@ -0,0 +1,17 @@
+from flask import render_template
+from app import app, db
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+
+    # Issue a session rollback to ensure any failed database sessions
+    # do not interfere with any database accesses triggered by
+    # the template. This resets the session to a clean state
+    db.session.rollback()
+    return render_template('500.html'), 500
