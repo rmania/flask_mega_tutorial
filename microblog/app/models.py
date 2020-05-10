6bb20d6e04c8927e46f0b53ee38aab0e6278953b
rmania <dmeijerink1979@gmail.com>
2020-05-10 17:42:41 +0200

    added avatars, about me blocks

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -3,6 +3,7 @@ from app import db
 from app import login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 
 # db.Model is baseclass from Flask-SQLAlchemy
@@ -27,11 +28,21 @@ class User(UserMixin, db.Model):
     def load_user(id):
         return User.query.get(int(id))
 
+    def avatar(self, size):
+        """
+        https://en.gravatar.com/site/implement/images
+        """
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # !
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f'<Post {self.body}>'
